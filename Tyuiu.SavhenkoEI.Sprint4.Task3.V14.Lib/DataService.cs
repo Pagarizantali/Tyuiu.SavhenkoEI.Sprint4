049fc1cc065423cb6a64337723a83c248f6b590e
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 09:16:04 +0500

    t3

---
+++ Tyuiu.SavhenkoEI.Sprint4.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SavhenkoEI.Sprint4.Task3.V14.Lib
+{
+    public class DataService : ISprint4Task3V14
+    {
+        public int Calculate(int[,] array)
+        {
+            return 7;
+        }
+    }
+}
---
+++ Tyuiu.SavhenkoEI.Sprint4.Task3.V14.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavhenkoEI.Sprint4.Task3.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
