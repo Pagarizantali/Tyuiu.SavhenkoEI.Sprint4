ee5be0b5cf6d11d5bfed0a7f09700e1b57152231
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 08:52:01 +0500

    t2

---
+++ Tyuiu.SavhenkoEI.Sprint4.Task1.V13.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SavhenkoEI.Sprint4.Task1.V13.Lib
+{
+    public class DataService : ISprint4Task1V13
+    {
+        public int Calculate(int[] array)
+        {
+            int sum = 0;
+            foreach (int item in array)
+            {
+                sum += item % 2 == 0 ? item : 0;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.SavhenkoEI.Sprint4.Task1.V13/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavhenkoEI.Sprint4.Task1.V13
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
