5b71163f2cc3f032438e20e64700479502564681
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 08:39:43 +0500

    Add project files.

---
+++ Tyuiu.SavchenkoEI.Sprint4.Task0.V27.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SavchenkoEI.Sprint4.Task0.V27.Lib
+{
+    public class DataService : ISprint4Task0V27
+    {
+        public int GetMultOddArrEl(int[] array)
+        {
+            int res = 1;
+            foreach (int numbers in array)
+            {
+                if (numbers % 2 == 1)
+                {
+                    res *= numbers;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint4.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint4.Task0.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
